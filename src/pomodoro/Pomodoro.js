572ae4687dc84e61252913465abd5ea924e21980
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2022-02-03 17:41:27 -0600

    Add files via upload

---
+++ src/pomodoro/Pomodoro.js
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import reactDomTestUtilsProductionMin from "react-dom/cjs/react-dom-test-utils.production.min";
+import classNames from "../utils/class-names";
+import useInterval from "../utils/useInterval";
+import Timer from "./Timer";
+import BreakButton from "./BreakButton";
+import FocusButton from "./FocusButton"
+import Stop from "./Stop";
+
+// These functions are defined outside of the component to ensure they do not have access to state
+// and are, therefore, more likely to be pure.
+
+/**
+ * Update the session state with new state after each tick of the interval.
+ * @param prevState
+ *  the previous session state
+ * @returns
+ *  new session state with timing information updated.
+ */
+function nextTick(prevState) {
+  const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
+  return {
+    ...prevState,
+    timeRemaining,
+  };
+}
+
+/**
+ * Higher-order function that returns a function to update the session state with the next session type upon timeout.
+ * @param focusDuration
+ *    the current focus duration
+ * @param breakDuration
+ *    the current break duration
+ * @returns
+ *  function to update the session state.
+ */
+function nextSession(focusDuration, breakDuration) {
+  /**
+   * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
+   */
+  return (currentSession) => {
+    if (currentSession.label === "Focusing") {
+      return {
+        label: "On Break",
+        timeRemaining: breakDuration * 60,
+      };
+    }
+    return {
+      label: "Focusing",
+      timeRemaining: focusDuration * 60,
+    };
+  };
+}
+
+function Pomodoro() {
+  // Timer starts out paused
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  // The current session - null where there is no session running
+  const [session, setSession] = useState(null);
+  const [focusDuration, changeFocus] = React.useState(25);
+  const [breakDuration, changeBreak] = React.useState(5);
+
+  /**
+   * Custom hook that invokes the callback function every second
+   *
+   * NOTE: You won't need to make changes to the callback function
+   */
+  useInterval(() => {
+      if (session.timeRemaining === 0) {
+        new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
+        return setSession(nextSession(focusDuration, breakDuration));
+      }
+      return setSession(nextTick);
+    },
+    isTimerRunning ? 1000 : null
+  );
+
+  /**
+   * Called whenever the play/pause button is clicked.
+   */
+  function playPause() {
+    setIsTimerRunning((prevState) => {
+      const nextState = !prevState;
+      if (nextState) {
+        setSession((prevStateSession) => {
+          // If the timer is starting and the previous session is null,
+          // start a focusing session.
+          if (prevStateSession === null) {
+            return {
+              label: "Focusing",
+              timeRemaining: focusDuration * 60,
+            };
+          }
+          return prevStateSession;
+        });
+      }
+      return nextState;
+    });
+  }
+
+  return (
+    <div className="pomodoro">
+      <div className="row">
+        <FocusButton 
+          focusDuration={focusDuration}
+          changeFocus={changeFocus}
+        />
+        <BreakButton 
+          breakDuration={breakDuration}
+          changeBreak={changeBreak}
+        />
+      </div>
+      <div className="row">
+        <div className="col">
+          <div
+            className="btn-group btn-group-lg mb-2"
+            role="group"
+            aria-label="Timer controls"
+          >
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-testid="play-pause"
+              title="Start or pause timer"
+              onClick={playPause}
+            >
+              <span
+                className={classNames({
+                  oi: true,
+                  "oi-media-play": !isTimerRunning,
+                  "oi-media-pause": isTimerRunning,
+                })}
+              />
+            </button>
+            <Stop 
+              isTimerRunning={isTimerRunning}
+              session={session}
+              setSession={setSession}
+              setIsTimerRunning={setIsTimerRunning}
+            />
+          </div>
+        </div>
+      </div>
+      <Timer 
+        session={session}
+        isTimerRunning={isTimerRunning}
+        focusDuration={focusDuration}
+        breakDuration={breakDuration}
+      />
+    </div>
+  );
+}
+
+export default Pomodoro;
